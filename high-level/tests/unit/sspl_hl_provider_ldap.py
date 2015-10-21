86e164b935328314c315737df39b77d062296fbb
Harshada Tupe <harshada.tupe@seagate.com>
2015-10-21 14:20:30 +0530

    CASTOR-879: Unit test for admin user command
    Change-Id: I5c8ff0ae12c0a63d1a05e377875fbbcd8af19ade

--- high-level/tests/unit/sspl_hl_provider_ldap.py
+++ high-level/tests/unit/sspl_hl_provider_ldap.py
@@ -2,9 +2,11 @@
 
 
 import unittest
-
+from mock import MagicMock
+from mock import patch
 from sspl_hl.providers.ldap.provider import LdapProvider
 from base_unit_test import BaseUnitTest
+
 
 # pylint: disable=too-many-public-methods
 
@@ -14,18 +16,55 @@ class SsplHlProviderLdap(BaseUnitTest):
         sspl_hl.providers.ldap.provider.LdapProvider object.
     """
 
+    # pylint: disable=no-self-use
+    @patch('ldap.initialize')
+    def test_ldapadminshow_queries(self, mock_obj):
+        """
+            Ensure that the arguments passed to show
+            command are taken by provider.
+        """
+        mocked_inst = mock_obj.return_value
+        mocked_inst.search_s = MagicMock()
+        self._query_provider(
+            args={
+                'command': 'user',
+                'subcommand': 'show',
+                'user': 'Victor@Clogeny'
+            },
+            provider=LdapProvider('LdapProvider', '')
+            )
+        self.assertTrue(mocked_inst.search_s.called)
+
+    @patch('ldap.initialize')
+    def test_ldapadminlist_queries(self, mock_obj_n):
+        """
+            Ensure list command will return all records in LDAP
+        """
+        mocked_inst_n = mock_obj_n.return_value
+        mocked_inst_n.search_s = MagicMock()
+        self._query_provider(
+            args={
+                'command': 'user',
+                'subcommand': 'list',
+                'user': 'all'
+            },
+            provider=LdapProvider('LdapProvider', '')
+            )
+        self.assertTrue(mocked_inst_n.search_s.called)
+
     def test_bad_command(self):
         """ Ensure sending a bad command results in an http error code.
-
         The cli should prevent this from happening, so this is just to cover
         the case of the user bypassing the cli and accessing the data provider
         directly.
         """
-        command_args = {'command': 'invalid_command', 'subcommand': 'show'}
-        self._query_provider(
-            args=command_args,
-            provider=LdapProvider('LdapProvider', '')
-        )
+        command_args = {'command': 'invalid_command', 'subcommand': 'show',
+                        'user': 'Victor@Clogeny'}
+        response_msg = "Error: Invalid command: 'invalid_command'"
+
+        self._test_args_validation_cases(command_args,
+                                         response_msg,
+                                         LdapProvider('LdapProvider', ''))
 
     def test_bad_subcommand(self):
         """ Ensure sending a bad subcommand results in an http error code.
