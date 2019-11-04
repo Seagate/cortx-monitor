a5e840a1afbfd67dfaa91f5854c859d161483b10
Malhar Vora <malhar.vora@seagate.com>
2019-11-04 14:02:23 +0530

    EOS-2739: UnboundLocalError: local variable 'uuid' referenced before assignment
    Change-Id: I029c390ef26416ce4d68ff27689d2c309e8d484b

--- low-level/framework/rabbitmq/rabbitmq_ingress_processor.py
+++ low-level/framework/rabbitmq/rabbitmq_ingress_processor.py
@@ -14,50 +14,48 @@
  ****************************************************************************
 """
 
-import pika
+import ctypes
 import json
 import os
 import time
 
+import pika
+
 from jsonschema import Draft3Validator
 from jsonschema import validate
-
-from pika import exceptions
-
 from framework.base.module_thread import ScheduledModuleThread
 from framework.base.internal_msgQ import InternalMsgQ
 from framework.utils.service_logging import logger
 from framework.rabbitmq.rabbitmq_egress_processor import RabbitMQegressProcessor
-
 from json_msgs.messages.actuators.ack_response import AckResponseMsg
 
-import ctypes
 try:
-    use_security_lib=True
+    use_security_lib = True
     SSPL_SEC = ctypes.cdll.LoadLibrary('libsspl_sec.so.0')
 except Exception as ae:
-    logger.info("libsspl_sec not found, disabling authentication on ingress msgs")
-    use_security_lib=False
+    logger.info(
+        "libsspl_sec not found, disabling authentication on ingress msgs")
+    use_security_lib = False
 
 
 class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
     """Handles incoming messages via rabbitMQ"""
 
     MODULE_NAME = "RabbitMQingressProcessor"
-    PRIORITY    = 1
+    PRIORITY = 1
 
     # Section and keys in configuration file
-    RABBITMQPROCESSOR     = MODULE_NAME.upper()
+    RABBITMQPROCESSOR = MODULE_NAME.upper()
     PRIMARY_RABBITMQ_HOST = 'primary_rabbitmq_host'
-    EXCHANGE_NAME         = 'exchange_name'
-    QUEUE_NAME            = 'queue_name'
-    ROUTING_KEY           = 'routing_key'
-    VIRT_HOST             = 'virtual_host'
-    USER_NAME             = 'username'
-    PASSWORD              = 'password'
+    EXCHANGE_NAME = 'exchange_name'
+    QUEUE_NAME = 'queue_name'
+    ROUTING_KEY = 'routing_key'
+    VIRT_HOST = 'virtual_host'
+    USER_NAME = 'username'
+    PASSWORD = 'password'
 
     JSON_ACTUATOR_SCHEMA = "SSPL-LL_Actuator_Request.json"
-    JSON_SENSOR_SCHEMA   = "SSPL-LL_Sensor_Request.json"
+    JSON_SENSOR_SCHEMA = "SSPL-LL_Sensor_Request.json"
 
     @staticmethod
     def name():
@@ -80,6 +78,16 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
                                    'schemas', 'sensors',
                                    self.JSON_SENSOR_SCHEMA)
         self._sensor_schema = self._load_schema(schema_file)
+
+        self._virtual_host = None
+        self._queue_name = None
+        self._routing_key = None
+        self._username = None
+        self._connection = None
+        self._primary_rabbitmq_host = None
+        self._exchange_name = None
+        self._password = None
+        self._channel = None
 
     def _load_schema(self, schema_file):
         """Loads a schema from a file and validates
@@ -112,12 +120,12 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
         # Display values used to configure pika from the config file
         self._log_debug("RabbitMQ user: %s" % self._username)
         self._log_debug("RabbitMQ exchange: %s, routing_key: %s, vhost: %s" %
-                      (self._exchange_name, self._routing_key, self._virtual_host))
+                        (self._exchange_name, self._routing_key, self._virtual_host))
 
     def run(self):
         """Run the module periodically on its own thread."""
-        #self._set_debug(True)
-        #self._set_debug_persist(True)
+        # self._set_debug(True)
+        # self._set_debug_persist(True)
 
         time.sleep(180)
         logger.info("RabbitMQingressProcessor, Initialization complete, accepting requests")
@@ -125,10 +133,10 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
         try:
             result = self._channel.queue_declare(exclusive=True)
             self._channel.queue_bind(exchange=self._exchange_name,
-                               queue=result.method.queue,
-                               routing_key=self._routing_key)
+                                     queue=result.method.queue,
+                                     routing_key=self._routing_key)
             self._channel.basic_consume(self._process_msg,
-                                  queue=result.method.queue)
+                                        queue=result.method.queue)
             self._channel.start_consuming()
 
         except Exception as e:
@@ -146,6 +154,7 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
         """Parses the incoming message and hands off to the appropriate module"""
 
         ingressMsg = {}
+        uuid = None
         try:
             if isinstance(body, dict) == False:
                 ingressMsg = json.loads(body)
@@ -153,11 +162,11 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
                 ingressMsg = body
 
             # Authenticate message using username and signature fields
-            username  = ingressMsg.get("username")
+            username = ingressMsg.get("username")
             signature = ingressMsg.get("signature")
-            message   = ingressMsg.get("message")
-            uuid      = ingressMsg.get("uuid")
-            msg_len   = len(message) + 1
+            message = ingressMsg.get("message")
+            uuid = ingressMsg.get("uuid")
+            msg_len = len(message) + 1
 
             if uuid is None:
                 uuid = "N/A"
@@ -181,7 +190,8 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
                 validate(ingressMsg, self._sensor_schema)
 
             else:
-                # We only handle incoming actuator and sensor requests, ignore everything else
+                # We only handle incoming actuator and sensor requests, ignore
+                # everything else.
                 return
 
             # Check for debugging being activated in the message header
@@ -214,7 +224,7 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
                 self._write_internal_msgQ(RabbitMQegressProcessor.name(), ack_msg)
 
             # Acknowledge message was received
-            ch.basic_ack(delivery_tag = method.delivery_tag)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
 
         except Exception as ex:
             logger.exception("RabbitMQingressProcessor, _process_msg unrecognized message: %r" % ingressMsg)
@@ -223,29 +233,35 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
 
     def _configure_exchange(self):
         """Configure the RabbitMQ exchange with defaults available"""
+        # Make methods locally available
+        get_value_with_default = self._conf_reader._get_value_with_default
         try:
-            self._virtual_host  = self._conf_reader._get_value_with_default(self.RABBITMQPROCESSOR,
-                                                                 self.VIRT_HOST,
-                                                                 'SSPL')
-            self._primary_rabbitmq_host = self._conf_reader._get_value_with_default(self.RABBITMQPROCESSOR,
-                                                                 self.PRIMARY_RABBITMQ_HOST,
-                                                                 'localhost')
-            self._exchange_name = self._conf_reader._get_value_with_default(self.RABBITMQPROCESSOR,
-                                                                 self.EXCHANGE_NAME,
-                                                                 'sspl-in')
-            self._queue_name    = self._conf_reader._get_value_with_default(self.RABBITMQPROCESSOR,
-                                                                 self.QUEUE_NAME,
-                                                                 'actuator-req-queue')
-            self._routing_key   = self._conf_reader._get_value_with_default(self.RABBITMQPROCESSOR,
-                                                                 self.ROUTING_KEY,
-                                                                 'actuator-req-key')
-            self._username      = self._conf_reader._get_value_with_default(self.RABBITMQPROCESSOR,
-                                                                 self.USER_NAME,
-                                                                 'sspluser')
-            self._password      = self._conf_reader._get_value_with_default(self.RABBITMQPROCESSOR,
-                                                                 self.PASSWORD,
-                                                                 'sspl4ever')
-            # ensure the rabbitmq queues/etc exist
+
+            self._virtual_host = get_value_with_default(self.RABBITMQPROCESSOR,
+                                                        self.VIRT_HOST, 'SSPL')
+
+            self._primary_rabbitmq_host = get_value_with_default(
+                self.RABBITMQPROCESSOR, self.PRIMARY_RABBITMQ_HOST,
+                'localhost')
+
+            self._exchange_name = get_value_with_default(
+                self.RABBITMQPROCESSOR, self.EXCHANGE_NAME, 'sspl-in')
+
+            self._queue_name = get_value_with_default(
+                self.RABBITMQPROCESSOR, self.QUEUE_NAME, 'actuator-req-queue')
+
+            self._routing_key = get_value_with_default(
+                self.RABBITMQPROCESSOR, self.ROUTING_KEY, 'actuator-req-key')
+
+            self._username = get_value_with_default(self.RABBITMQPROCESSOR,
+                                                    self.USER_NAME,
+                                                    'sspluser')
+
+            self._password = get_value_with_default(self.RABBITMQPROCESSOR,
+                                                    self.PASSWORD,
+                                                    'sspl4ever')
+
+            # Ensure the rabbitmq queues/etc exist
             creds = pika.PlainCredentials(self._username, self._password)
             self._connection = pika.BlockingConnection(
                 pika.ConnectionParameters(
@@ -276,7 +292,8 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
                 routing_key=self._routing_key
                 )
         except Exception as ex:
-            logger.exception("RabbitMQingressProcessor, _configure_exchange: %r" % ex)
+            logger.exception("RabbitMQingressProcessor, \
+                _configure_exchange: %r" % ex)
 
     def shutdown(self):
         """Clean up scheduler queue and gracefully shutdown thread"""
@@ -285,4 +302,5 @@ class RabbitMQingressProcessor(ScheduledModuleThread, InternalMsgQ):
             self._connection.close()
             self._channel.stop_consuming()
         except pika.exceptions.ConnectionClosed:
-            logger.info("RabbitMQingressProcessor, shutdown, RabbitMQ ConnectionClosed")
+            logger.info("RabbitMQingressProcessor, shutdown, \
+                RabbitMQ ConnectionClosed")
