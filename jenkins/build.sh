c0146c1b6d467402ddc7297ced49a558329a5791
Sumedh Kulkarni <sumedh.a.kulkarni@seagate.com>
2020-09-11 05:12:03 -0600

    modified config files regarding SNMP_traps2 sensor

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -140,7 +140,7 @@ python3 -m pip install --user -r $req_file  > /dev/null || {
 #Check & install required RPM packages
 echo "Installing build required RPM packages..."
 yum install -y python36-dbus python36-paramiko \
-    python36-psutil python36-gobject eos-py-utils pysnmp
+    python36-psutil python36-gobject eos-py-utils
 
 echo "Generating tar & RPM's for pre requisite packages systemd_python."
 yum erase -y systemd-python36-*
