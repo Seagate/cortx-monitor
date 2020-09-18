a09863b99b4ba790213ee8b0142d87870daf7a47
Shubham Bhosale <shubham.bhosale@seagate.com>
2020-09-18 02:50:49 -0600

    EOS:12733 SSPL: JBOD: Populate list of unsupported features
    Signed-off-by: Shubham Bhosale <shubham.bhosale@seagate.com>

--- low-level/files/opt/seagate/sspl/bin/sspl_fetch_unsupported_list.py
+++ low-level/files/opt/seagate/sspl/bin/sspl_fetch_unsupported_list.py
@@ -2,6 +2,9 @@ import subprocess
 import ast
 from eos.utils.product_features import unsupported_features
 
+storage_type = "physical"
+server_type = "Virtual"
+
 try:
     setup_info = subprocess.Popen(['sudo', 'provisioner', 'get_setup_info'],
         stdout=subprocess.PIPE).communicate()[0].decode("utf-8").rstrip()
@@ -11,8 +14,10 @@ try:
     print(f"Storage Type : '{storage_type}'")
     print(f"Server Type '{server_type}'")
     if (storage_type=="virtual") and (server_type=="virtual"):
+        print("Unsupported features adding into unsupported feature Database.")
         unsupported_feature_instance = unsupported_features.UnsupportedFeaturesDB()
         unsupported_feature_instance.store_unsupported_features(component_name="sspl", features=["health"])
+        print("Unsupported feature list added sucessfully.")
 except Exception as err:
     print(f"Error in getting setup information : {err}")
     print(f"Considering default storage type : '{storage_type}'")
