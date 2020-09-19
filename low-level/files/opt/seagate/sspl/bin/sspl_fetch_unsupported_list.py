3ebbc88c3a1d42ffb9a3fd2a30f4d917c22be880
Shubham Bhosale <shubham.bhosale@seagate.com>
2020-09-18 23:49:19 -0600

    EOS:12733 SSPL: JBOD: Populate list of unsupported features
    Signed-off-by: Shubham Bhosale <shubham.bhosale@seagate.com>

--- low-level/files/opt/seagate/sspl/bin/sspl_fetch_unsupported_list.py
+++ low-level/files/opt/seagate/sspl/bin/sspl_fetch_unsupported_list.py
@@ -1,10 +1,27 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2001-2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
 import subprocess
 import ast
-from eos.utils.product_features import unsupported_features
+from cortx.utils.product_features import unsupported_features
 
 storage_type = "virtual"
 server_type = "virtual"
-
+unsupported_feature_list = ["health"]
 try:
     setup_info = subprocess.Popen(['sudo', 'provisioner', 'get_setup_info'],
         stdout=subprocess.PIPE).communicate()[0].decode("utf-8").rstrip()
@@ -16,7 +33,7 @@ try:
     if (storage_type=="virtual") and (server_type=="virtual"):
         print("Unsupported features adding into unsupported feature Database.")
         unsupported_feature_instance = unsupported_features.UnsupportedFeaturesDB()
-        unsupported_feature_instance.store_unsupported_features(component_name="sspl", features=["health"])
+        unsupported_feature_instance.store_unsupported_features(component_name="sspl", features=unsupported_feature_list)
         print("Unsupported feature list added sucessfully.")
 except Exception as err:
     print(f"Error in getting setup information : {err}")
