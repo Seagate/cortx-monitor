02e276985bac5ad5744b3a5f8d68d092b8485571
MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>
2020-10-19 11:46:49 +0000

    EOS-14380: Remove dependency of Consul hardcoded path inside Hare and move to /usr/bin/consul - SSPL - (cortx-1.0)
    Signed-off-by: MARIYAPPAN PONNUSAMY <mariyappan.ponnusamy@seagate.com>

--- sspl_test/constants.sh
+++ sspl_test/constants.sh
@@ -15,5 +15,4 @@
 
 PRODUCT_NAME='LDR_R1'
 PRODUCT_FAMILY='cortx'
-CONSUL_PATH="/opt/seagate/$PRODUCT_FAMILY/hare/bin"
-CONSUL_FALLBACK_PATH="/opt/seagate/$PRODUCT_FAMILY/sspl/bin"
+CONSUL_PATH="/usr/bin/"
