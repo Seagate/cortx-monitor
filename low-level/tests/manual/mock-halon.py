88859061c7c39bfef041b8ae2e30549c97569b2e
Archana Limaye <archana.limaye@seagate.com>
2020-10-20 08:31:56 -0600

    Changes for EOSEOS-14403 Alert name issue on GUI: instead of memory alert, alert triggered as system alert.
    Modified the RESOURCE_TYPE to give more information about the resource
    
    Signed-off-by: Archana Limaye <archana.limaye@seagate.com>

--- low-level/tests/manual/mock-halon.py
+++ low-level/tests/manual/mock-halon.py
@@ -131,7 +131,7 @@ if __name__ == "__main__":
             },
             "sensor_request_type": {
                 "node_data": {
-                    "sensor_type": "node:os:system"
+                    "sensor_type": "node:os:system_memory"
                 }
             }
         }
