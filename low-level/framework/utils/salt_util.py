f2865aa9568cbaf8708d9ad1a6bda4bc9143b5d2
Mohit Pathak <mohit.pathak@seagate.com>
2020-11-04 22:46:28 -0700

    Store minion id, consul host and port in conf file during sspl_config
    Signed-off-by: Mohit Pathak <mohit.pathak@seagate.com>

--- low-level/framework/utils/salt_util.py
+++ low-level/framework/utils/salt_util.py
@@ -14,83 +14,51 @@
 # cortx-questions@seagate.com.
 
 
-'''Module which provides common config information using Provisioner API'''
+'''Module which provides common config information using cortx utils'''
 
-
-import errno
-
-try:
-    from service_logging import logger
-    from utility import Utility
-except Exception as err:
-    from framework.utils.service_logging import logger
-    from framework.utils.utility import Utility
-
+import configparser
+from cortx.utils.kvstore.pillar import PillarStorage
+import subprocess
+import sys
 
 class SaltInterface:
     """
     Interface class to get common config from common pillar using
     Provisioner API
     """
-
-    __instance = None
-    SALT_FILE = '/etc/salt/minion_id'
-
     CLUSTER_KEY = 'cluster'
     CLUSTER_TYPE_KEY = 'type'
     SSPL_KEY = 'sspl'
     DATASTORE_KEY = 'DATASTORE'
+    SYSTEM_INFORMATION_KEY = 'SYSTEM_INFORMATION'
+    MINION_ID_KEY = 'salt_minion_id'
+    CONSUL_HOST_KEY = 'consul_host'
+    CONSUL_PORT_KEY = 'consul_port'
 
     def __init__(self):
         '''init method'''
-        if SaltInterface.__instance is None:
-            self.utility = Utility()
-            self.pillar_info = None
-            self.node_id = None or self.get_node_id()
-            _result, _err, _ret_code = \
-                    self.utility.execute_cmd(['sudo', 'provisioner', 'pillar_get'])
-            _err = _err.decode("utf-8").rstrip()
-            if _ret_code == 0 and _err == '':
-                self.pillar_info = eval(_result.decode("utf-8").rstrip())
-            self._is_single_node = None or self._is_server_single(_err)
-            self.consul_host = None or self.get_consul_vip(_err)
-            self.consul_port = None or self.get_consul_port(_err)
-            SaltInterface.__instance = self
+        self.node_id = self.get_node_id()
+        self.pillar_storage = PillarStorage()
+        self._is_single_node = self._is_server_single()
+        self.consul_host = self.get_consul_vip()
+        self.consul_port = self.get_consul_port()
+        self.config = configparser.ConfigParser()
 
-    @staticmethod
-    def get_singleton_instance():
-        '''Returns an instance of this class'''
-        if SaltInterface.__instance is None:
-            SaltInterface()
-        return SaltInterface.__instance
-
-    @staticmethod
-    def get_node_id():
+    def get_node_id(self):
         '''Returns salt minion_id using salt config file'''
 
         _node_id = 'srvnode-1'
         try:
-            with open(SaltInterface.SALT_FILE, 'r') as salt_file:
-                _node_id = salt_file.read().rstrip()
-            if _node_id is None or _node_id == '':
-                _node_id = 'srvnode-1'
-            logger.info(f'salt_util, Server minion ID: {_node_id}')
-        except OSError as err:
-            if err.errno == errno.ENOENT:
-                logger.error(
-                    f'Problem occured while reading from salt file. \
-                     File path: {SaltInterface.SALT_FILE} does not exist')
-            elif err == errno.EACCES:
-                logger.error(
-                    f'Problem occured while reading from salt file. \
-                     Permission issue while reading from: {SaltInterface.SALT_FILE}')
+            CMD = ['sudo', 'salt-call', '--local', 'grains.get', 'id', '--output=newline_values_only']
+            _result, _err, _ret_code = self.execute_cmd(CMD)
+            if _ret_code == 0 and _err.decode('utf-8') == '':
+                _node_id = _result.decode('utf-8').rstrip()
             else:
-                logger.error(
-                    f'Problem occured while reading from salt file with \
-                     the issue: {err}')
+                print(f'salt_util, Failed to read node-id with error : {_err},' + \
+                             f'Keeping default value as srvnode-1')
         except Exception as err:
-            logger.warning(f'salt_util, Fail to read node-id with error: {err}, \
-                             Keeping default value as srvnode-1')
+            print(f'salt_util, Failed to read node-id with error: {err},' + \
+                             f'Keeping default value as srvnode-1')
         return _node_id
 
     def _is_server_single(self, _err=None):
@@ -100,28 +68,49 @@ class SaltInterface:
         _is_single_node = False
 
         try:
-            if self.pillar_info:
-                cluster_type = \
-                    self.pillar_info[self.node_id][self.CLUSTER_KEY][self.CLUSTER_TYPE_KEY]
-                if cluster_type is not None and cluster_type.lower() == 'single':
-                    _is_single_node = True
-                logger.info(f'salt_util, Node type: {_is_single_node}')
-            else:
-                logger.warning(f'salt_util, Fail to read cluster type with \
-                                 this error: {_err}. Assuming single node')
-        except KeyError as key_err:
-            logger.warning(f'salt_util, Fail to read cluster type with \
-                             this key error: {key_err}, Assuming single node')
+            if self.pillar_storage.get(f'{self.CLUSTER_KEY}:{self.CLUSTER_TYPE_KEY}') == 'single':
+                _is_single_node = True
         except Exception as err:
-            logger.warning(f'salt_util, Fail to read cluster type with \
-                             this error: {err}, Assuming single node')
+            print(f'salt_util, Fail to read cluster type with' + \
+                             f'this error: {err}, Assuming multi node')
         return _is_single_node
+
+    def is_env_vm(self):
+        """
+        Returns true if VM env, false otherwise
+        """
+        is_vm = True
+        CMD = "facter is_virtual"
+        try:
+            subout = subprocess.Popen(CMD, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result = subout.stdout.readlines()
+            if result == [] or result == "":
+                print("Not able to read whether env is vm or not, assuming VM env.")
+            else:
+                if 'false' in result[0].decode():
+                    is_vm = False
+        except Exception as e:
+            print(f"Error while reading whether env is vm or not, assuming VM env : {e}")
+        return is_vm
+
+    def execute_cmd(self, command=[]):
+        """
+        Executes action using python subprocess module
+        and returns the output
+        1. output of the command in byte
+        2. return_code
+        """
+        #TODO Handle exception at caller side
+        process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE,\
+                                   stderr=subprocess.PIPE)
+        _result, _err = process.communicate()
+        return _result, _err, process.returncode
 
     def get_consul_vip(self, _err=None):
         """
         Returns IP used to connect to Consul
         """
-        _is_env_vm = self.utility.is_env_vm()
+        _is_env_vm = self.is_env_vm()
         _is_single_server = self._is_single_node
 
         # Initialize to localhost
@@ -130,23 +119,12 @@ class SaltInterface:
         # Get vip if not a vm or single node, default to localhost otherwise
         if not _is_env_vm or not _is_single_server:
             try:
-                if self.pillar_info is not None:
-                    _consul_vip = \
-                        self.pillar_info[self.node_id][self.CLUSTER_KEY][self.node_id]\
-                        ['network']['data_nw']['roaming_ip']
-                    logger.debug(f'salt_util, consul VIP: {_consul_vip}')
-                else:
-                    logger.warning(f'salt_util, Fail to read consul VIP with \
-                                     this error: {_err}, Assuming localhost: \
-                                     {_consul_vip} connection')
-            except KeyError as key_err:
-                logger.warning(f'salt_util, fail to read consul vip with \
-                                 this key error: {key_err}, assuming localhost: \
-                                 {_consul_vip} connection')
+                _consul_vip = self.pillar_storage.get(f'{self.CLUSTER_KEY}:{self.node_id}:' \
+                    + 'network:data_nw:roaming_ip')
             except Exception as err:
-                logger.warning(f'salt_util, fail to read consul vip with \
-                                 this error: {err}, assuming localhost: \
-                                 {_consul_vip} connection')
+                print(f'salt_util, fail to read consul vip with' + \
+                                 f'this error: {err}, assuming localhost:' + \
+                                f'{_consul_vip} connection')
         return _consul_vip
 
     def get_consul_port(self, _err=None):
@@ -157,26 +135,37 @@ class SaltInterface:
         _consul_port = '8500'
         # Read consul port from sspl.sls
         try:
-            if self.pillar_info is not None:
-                data_store_config = \
-                    self.pillar_info[self.node_id][self.SSPL_KEY][self.DATASTORE_KEY]
-                if data_store_config is not None:
-                    _consul_port = data_store_config['consul_port']
-                logger.info(f'salt_util, consul port: {_consul_port}')
-            else:
-                logger.warning(f'salt_util, Fail to read consul port with \
-                                 this error: {_err}, Assuming the port: {_consul_port}')
-        except KeyError as key_err:
-            logger.warning(f'salt_util, Fail to read consul port with \
-                             this error: {key_err}, Assuming the port: {_consul_port}')
+            _consul_port = self.pillar_storage.get(f'{self.SSPL_KEY}:{self.DATASTORE_KEY}:consul_port')
         except Exception as err:
-            logger.warning(f'salt_util, Fail to read consul port with \
-                             this error: {err}, Assuming the port: {_consul_port}')
+            print(f'salt_util, Fail to read consul port with' + \
+                             f'this error: {err}, Assuming the port: {_consul_port}')
         return _consul_port
 
+    def update_config_file(self, conf_file):
+        """
+        Writes salt minion id, consul host and port to conf file
+        """
+        try:
+            # Read the config
+            self.config.read(conf_file)
+            # Update required values
+            if self.SYSTEM_INFORMATION_KEY in self.config.sections():
+                self.config[self.SYSTEM_INFORMATION_KEY][self.MINION_ID_KEY] = self.node_id
+            else:
+                print(f'salt_util, Failed to update minion id in {conf_file}')
+            if self.DATASTORE_KEY in self.config.sections():
+                self.config[self.DATASTORE_KEY][self.CONSUL_HOST_KEY] = self.consul_host
+                self.config[self.DATASTORE_KEY][self.CONSUL_PORT_KEY] = self.consul_port
+            else:
+                print(f'salt_util, Failed to update consul host and port in {conf_file}')
+            # Write the config to file
+            with open(conf_file, 'w') as configfile:
+                self.config.write(configfile)
+        except Exception as err:
+            print(f'salt_util, Failed to update conf_file {conf_file} with error : {err}')
 
-salt_util = SaltInterface.get_singleton_instance()
-if salt_util:
-    node_id = salt_util.node_id
-    consulhost = salt_util.consul_host
-    consulport = salt_util.consul_port
+
+if __name__ == "__main__":
+    conf_file = sys.argv[1]
+    salt_util = SaltInterface()
+    salt_util.update_config_file(conf_file)
