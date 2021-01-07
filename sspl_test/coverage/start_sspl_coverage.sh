4b6fe7d7fc4e8f39a96e7b2a65015e78faca10bf
Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>
2021-01-06 22:40:10 -0700

    EOS-15983: Automate code coverage (added scripts and edited sspl_ll_d)
    Signed-off-by: Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>

---
+++ sspl_test/coverage/start_sspl_coverage.sh
@@ -0,0 +1,55 @@
+#! /bin/sh
+
+echo "Stoping sspl-ll.service for setting up coverage"
+systemctl stop sspl-ll.service
+
+echo "Checking and installing coverage.py"
+pip3_status=$(which pip3)
+pip_status=$(which pip)
+if [[ $pip3_status =~ "/bin/pip3" ]]
+then 
+    pip3 install coverage
+elif [[$pip_status =~ "/bin/pip"]]
+then
+    pip install coverage
+fi
+
+echo "Creating required files for coverage.."
+iofdir='/opt/seagate/cortx/sspl/low-level/framework'
+# iofdir='/opt/sumedh/withoutpyi/cortx-sspl/low-level/framework'
+usdir='/opt/seagate/cortx/sspl/sspl_test/coverage'
+# usdir='/opt/sumedh/withoutpyi/cortx-sspl/sspl_test/coverage'
+
+sudo cp $iofdir/sspl_ll_d $iofdir/sspl_ll_d_coverage
+
+copy_lines() {
+    for line_num in `seq $1 $2` 
+    do 
+        str=`sed $((line_num))!d $usdir/coverage_code`; fix='\';
+        str="${fix}${str}";
+        curr_line=$((curr_line+1));
+        echo $curr_line $str;
+        sed -i "$curr_line i $str" $iofdir/sspl_ll_d_coverage;
+    done    
+}
+
+curr_line=`grep -n "# Creating Coverage instance for coverage report generation" $iofdir/sspl_ll_d_coverage | cut -d : -f1`
+copy_lines 1 7
+
+curr_line=`grep -n "#Staring coverage report scope" $iofdir/sspl_ll_d_coverage | cut -d : -f1`
+copy_lines 8 9
+
+curr_line=`grep -n "# Coverage report commands to stop, save and generate coverage report" $iofdir/sspl_ll_d_coverage | cut -d : -f1`
+copy_lines 10 25
+
+echo "Changing the soft link and adding permission for /tmp/sspl/ folder"
+ln -sf $iofdir/sspl_ll_d_coverage /usr/bin/sspl_ll_d
+
+chmod 777 /tmp/sspl/* 
+chown sspl-ll:sspl-ll /tmp/sspl/ -R
+
+echo "Staring the sspl-ll.service back.."
+systemctl start sspl-ll.service
+
+echo "Environment is set for testing.. "
+echo "Please execute stop_sspl_coverage.sh after testing so that coverage report is generated."
---
+++ sspl_test/coverage/stop_sspl_coverage.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# iofdir='/opt/sumedh/withoutpyi/cortx-sspl/low-level/framework'
+iofdir='/opt/seagate/cortx/sspl/low-level/framework'
+
+echo "Generating the coverage report.."
+consul kv put sspl/config/SYSTEM_INFORMATION/log_level DEBUG
+
+echo "The report is saved at /tmp/sspl/sspl_xml_coverage_report.xml"
+
+echo "Stoping sspl-ll for resetting the sspl environment"
+systemctl stop sspl-ll.service
+
+ln -sf $iofdir/sspl_ll_d /usr/bin/sspl_ll_d
+
+sudo rm $iofdir/sspl_ll_d_coverage
+
+consul kv put sspl/config/SYSTEM_INFORMATION/log_level INFO
+
+echo "Normal sspl environment is set staring the sspl-ll.service"
+systemctl start sspl-ll.service
+echo "Done."
