4f3f4fbce9010c0243ce6440262cadedeba1d51e
Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>
2021-01-07 22:43:39 -0700

    EOS-15983: Automate code coverage (updated the scripts)
    Signed-off-by: Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>

--- sspl_test/coverage/stop_sspl_coverage.sh
+++ sspl_test/coverage/stop_sspl_coverage.sh
@@ -5,7 +5,7 @@ target_dir='/opt/seagate/cortx/sspl/low-level/framework'
 echo "Generating the coverage report.."
 consul kv put sspl/config/SYSTEM_INFORMATION/log_level DEBUG
 sleep 30s
-timestamp=`stat /tmp/sspl/sspl_xml_coverage_report.xml | grep Modify | cut -d ' ' -f2,3`
+timestamp=$(stat /tmp/sspl/sspl_xml_coverage_report.xml | grep Modify | cut -d ' ' -f2,3)
 echo "${timestamp} : The report is saved at /tmp/sspl/sspl_xml_coverage_report.xml"
 
 echo "Stoping sspl-ll for resetting the sspl environment"
