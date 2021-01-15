d170917c4c8b77fd48f598c0d2cf55466ab74335
Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>
2021-01-15 04:29:14 -0700

    EOS-16522: sspl_setup_init to python (.sh removed .py added)
    Signed-off-by: Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>

--- low-level/files/opt/seagate/sspl/setup/sspl_setup_init.py
+++ low-level/files/opt/seagate/sspl/setup/sspl_setup_init.py
@@ -1,20 +1,43 @@
+#!/bin/env python3
+
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
+#################################################################
+# This script performs following operations.
+# - Creates datapath as defined in /etc/sspl.conf
+# - Check dependencies for roles other than '<product>'
+#################################################################
+
 import sys
 import os
 import subprocess
 import pwd
 
 sys.path.insert(0, '/opt/seagate/cortx/sspl/low-level/')
-# sys.path.insert(0, '/op/sumedh/cortx-sspl/low-level')
 
 from framework.base.sspl_constants import file_store_config_path, roles
 import psutil
 
 
 class Init:
+
     """Init Setup Interface"""
+
     name = "init"
 
-    ### not needed if <ssu | gw | cmu> removed
     SSU_DEPENDENCY_RPMS = [
                 "sg3_utils",
                 "gemhpi",
@@ -31,19 +54,22 @@ class Init:
                     "openhpid",
                     "dcs-collectord",
     ]
-    ########################################
 
     VM_DEPENDENCY_RPMS = []
 
+    hpi_path = '/tmp/dcs/hpi'
+    mdadm_path = '/etc/mdadm.conf'
+
     def __init__(self, args : list):
         self.args = args
+        self.role = None
 
     def usage(self):
         sys.stderr.write(
             f"{self.name} [check|create [-dp] [-r <ssu|gw|cmu|vm|cortx>]]\n"
             "create options:\n"
             "\t -dp Create configured datapath\n"
-            "\t -r  Role to be configured on the current node\n" 
+            "\t -r  Role to be configured on the current node\n"
             )
         sys.exit(1)
 
@@ -61,7 +87,7 @@ class Init:
 
     def check_for_dep_rpms(self, rpm_list : list):
         # there is a way to do it using 'yum' pkg but curretly not working with python3, working on python2
-        # inplemented using cmd command 'rpm -q pkgname' 
+        # implemented using cmd command 'rpm -q pkgname'
         for rpm in rpm_list:
             name = self._send_command('rpm -q ' + rpm)
             if 'not installed' in name:
@@ -76,9 +102,6 @@ class Init:
                 sys.exit(1)
 
     def check_dependencies(self, role : str):
-        ## function gets converted to single function call on removal of <ssu | cmu | gw> 
-        ## and that function is also not doing anything since self.VM_DEPENDENCY_RPMS is empty
-
         # Check for dependency rpms and required processes active state based on role
         if role == "ssu":
             print(f"Checking for dependency rpms for role '{role}'")
@@ -106,7 +129,7 @@ class Init:
         if not self.args:
             self.usage()
 
-        i =0
+        i = 0
         while i < len(self.args):
             if self.args[i] == '-dp':
                 # Extract the data path
@@ -118,7 +141,6 @@ class Init:
                             break
                 # Crete the directory and assign permissions
                 try:
-                    # what should be the permissions for this directory ??
                     os.makedirs(sspldp, mode=0o766, exist_ok=True)
                     sspl_ll_uid = self.get_uid('sspl-ll')
                     if sspl_ll_uid == -1:
@@ -130,7 +152,8 @@ class Init:
                         for item in files:
                             os.chown(os.path.join(root, item), sspl_ll_uid, -1)
                 except OSError as error:
-                    print(error) 
+                    print(error)
+                    sys.exit(1)
 
             elif self.args[i] == '-r':
                 i+=1
@@ -145,30 +168,28 @@ class Init:
         # Create /tmp/dcs/hpi if required. Not needed for '<product>' role
         if self.role != "cortx":
             try:
-                os.makedirs('/tmp/dcs/hpi', mode=0o777, exist_ok=True)
+                os.makedirs(self.hpi_path, mode=0o777, exist_ok=True)
                 zabbix_uid = self.get_uid('zabbix')
                 if zabbix_uid != -1:
-                    os.chown('/tmp/dcs/hpi', zabbix_uid, -1)
+                    os.chown(self.hpi_path, zabbix_uid, -1)
             except OSError as error:
-                print(error) 
+                print(error)
 
         # Check for sspl required processes and misc dependencies like installation, etc based on 'role'
-        if self.role : 
+        if self.role:
             self.check_dependencies(self.role)
         
         # Create mdadm.conf to set ACL on it.
-        with open('/etc/mdadm.conf', 'a'):
-            os.utime('/etc/mdadm.conf')
+        with open(self.mdadm_path, 'a'):
+            os.utime(self.mdadm_path)
 
-        # self._send_command('setfacl -m u:sspl-ll:rw /etc/mdadm.conf') 
-        #  What should be the file permissions need here and are `setfacl` and below impl equivalent??
-        os.chmod('/etc/mdadm.conf', mode=0o666)
+        # self._send_command('setfacl -m u:sspl-ll:rw /etc/mdadm.conf')
+        os.chmod(self.mdadm_path, mode=0o666)
         sspl_ll_uid = self.get_uid('sspl-ll')
         if sspl_ll_uid == -1:
             sys.exit(1)
-        os.chown('/etc/mdadm.conf', sspl_ll_uid, -1)
+        os.chown(self.mdadm_path, sspl_ll_uid, -1)
         
-
 if __name__ == "__main__":
     ic = Init(sys.argv[1:])
     ic.process()
