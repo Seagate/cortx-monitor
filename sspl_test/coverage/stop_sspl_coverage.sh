dff9ae83664db145b1574a4ac68c4affb30dfbce
Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>
2021-02-01 05:15:38 -0700

    <refactor>used SIGUSR1 signal to trigger, moved all code to covergae_code
    Signed-off-by: Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>

--- sspl_test/coverage/stop_sspl_coverage.sh
+++ sspl_test/coverage/stop_sspl_coverage.sh
@@ -3,7 +3,7 @@
 target_dir='/opt/seagate/cortx/sspl/low-level/framework'
 
 echo "Generating the coverage report.."
-systemctl --kill-who=main kill -s SIGRTMIN+0 sspl-ll.service
+systemctl --kill-who=main kill -s SIGUSR1 sspl-ll.service
 sleep 20s
 timestamp=$(stat /tmp/sspl/sspl_xml_coverage_report.xml | grep Modify | cut -d ' ' -f2,3)
 echo "${timestamp} : The report is saved at /tmp/sspl/sspl_xml_coverage_report.xml"
