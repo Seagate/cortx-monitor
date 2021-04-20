03d57ef663a5c1bbd6ea5bfea3027fb17fa033a7
Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>
2021-04-20 07:45:48 -0600

    EOS-19072-ImproveCoverage: added license and modified coverage_setup.py script.
    Signed-off-by: Sumedh A. Kulkarni <sumedh.a.kulkarni@seagate.com>

--- sspl_test/coverage/coverage_setup.py
+++ sspl_test/coverage/coverage_setup.py
@@ -1,15 +1,45 @@
 #! /usr/bin/python3
+
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Affero General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <https://www.gnu.org/licenses/>. For any questions
+# about this software or licensing, please email opensource@seagate.com or
+# cortx-questions@seagate.com.
+
+# NOTE : This script is written to replace start_sspl_coverage.sh and
+#        stop_sspl_coverage.sh bash scripts with python script in future.
+#        currently this file is not in use.
+
 import shutil
 import os
 import pwd
 import signal
 import psutil
 import time
+from cortx.utils.process import SimpleProcess
 
 file_path = "/opt/seagate/cortx/sspl/low-level"
 report_path = "/var/cortx/sspl/coverage/sspl_xml_coverage_report.xml"
 
 def coverage_setup():
+    """Installs pip3 coverage package. Injects different patches code from
+       coverage_code file to sspl_ll_d file, also creates target directory
+       for code coverage report and assigns permission to the directory.
+    """
+    print("Installing coverage.py")
+    _, _, return_code = SimpleProcess('python3 -m pip install coverage').run()
+    if return_code:
+        return return_code
+
     print("Creating required files for coverage..")
     patch1_name = "initialize coverage obj for code coverage report generation"
     patch1_start_ind = 0
@@ -66,17 +96,25 @@ def coverage_setup():
     os.makedirs('/var/cortx/sspl/coverage/', 0o755, exist_ok=True)
     os.chown('/var/cortx/sspl/coverage/', uid, -1)
 
+    return 0
+
 
 def coverage_reset():
+    """Send SIGUSR1 signal to sspl_ll_d to trigger code coverage report generation.
+       Swap modified sspl_ll_d file with original one.
+    """
     print("Generating the coverage report..")
     for proc in psutil.process_iter():
         if "sspl_ll_d" in proc.name():
             pid = proc.pid
+
     os.kill(pid, signal.SIGUSR1)
     time.sleep(5)
+
     modification_time = \
         os.path.getmtime(report_path)
-    if(time.time() - modification_time < 100):
+
+    if (time.time() - modification_time) < 100:
         modification_time = time.strftime('%Y-%m-%d %H:%M:%S',
                                           time.localtime(modification_time))
         print("%s : The Code Coverage Report is saved at %s" %
