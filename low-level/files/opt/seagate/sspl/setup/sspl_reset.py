8be171125952839f2a0c8bb7aca69ec052352dc3
Palak Garg <palak.garg@seagate.com>
2021-05-25 12:09:47 -0600

    EOS-19806: Need factory reset support through mini provisioner interface
    * Fixed flake8 issues.
    
    * Fixed Codacy issues.
    
    * Clean '.log.gz' format log files through reset interface.
    
    Signed-off-by: Palak Garg <palak.garg@seagate.com>

--- low-level/files/opt/seagate/sspl/setup/sspl_reset.py
+++ low-level/files/opt/seagate/sspl/setup/sspl_reset.py
@@ -2,89 +2,56 @@ import os
 import shutil
 
 from framework.base.sspl_constants import (PRODUCT_FAMILY,
-                                           file_store_config_path,
-                                           SSPL_CONFIGURED,
                                            DATA_PATH,
                                            SSPL_CONFIG_INDEX,
                                            sspl_config_path)
-from cortx.utils.process import SimpleProcess
 from cortx.utils.service import DbusServiceHandler
 from cortx.utils.conf_store import Conf
-from files.opt.seagate.sspl.setup.setup_error import SetupError
 from files.opt.seagate.sspl.setup.setup_logger import logger
+
 
 class HardReset:
     """Performs SSPL Hard reset.
-    'hard' is used to reset configs and clean log directory """
+    This Interface is used to reset configs and clean log files """
 
     name = "hard"
-    user_present=False
+    user_present = False
 
     def del_file(self, filename):
         if os.path.exists(filename):
             os.remove(filename)
 
+    @classmethod
+    def del_files_from_dir(cls, fformat, dir_path):
+        for root, _, files in os.walk(dir_path):
+            for file in files:
+                if file.endswith(fformat):
+                    os.remove(os.path.join(root, file))
+
     def process(self):
-        # stop sspl service
         dbus_service = DbusServiceHandler()
-        dbus_service.stop('sspl-ll.service')
-
-        # Remove sspl_conf
-        self.del_file(file_store_config_path)
-
-        # Remove sspl-configured file
-        self.del_file(SSPL_CONFIGURED)
+        # Stop SSPL service if state is active
+        service_state = dbus_service.get_state('sspl-ll.service')
+        if service_state._state == 'active':
+            logger.info(f'For {self.name}, SSPL service should be stopped'
+                        'through HA interfaces')
+            logger.info("Stopping SSPL service now...")
+            dbus_service.stop('sspl-ll.service')
 
         # Remove sspl data
         shutil.rmtree(DATA_PATH, ignore_errors=True)
 
-        # Remove sspl-ll user if preset
-        CMD = "id -u sspl-ll"
-        output, error, returncode = SimpleProcess(CMD).run()
-        if returncode != 0:
-            msg = "ERROR: %s - CMD %s" % (error, CMD)
-            logger.error(msg)
-            raise SetupError(returncode, msg)
-        else:
-            self.user_present=True
+        # Remove log files
+        HardReset.del_files_from_dir('.log', f"/var/log/{PRODUCT_FAMILY}/sspl/")
+        HardReset.del_files_from_dir('.log.gz',
+                                     f"/var/log/{PRODUCT_FAMILY}/sspl/")
+        HardReset.del_files_from_dir('.log', f"/var/log/{PRODUCT_FAMILY}/iem/")
+        HardReset.del_files_from_dir('.log.gz',
+                                     f"/var/log/{PRODUCT_FAMILY}/iem/")
 
-        if self.user_present:
-            CMD="/usr/sbin/userdel sspl-ll"
-            output, error, returncode = SimpleProcess(CMD).run()
-            if returncode != 0:
-                msg = "ERROR: %s - CMD %s" % (error, CMD)
-                logger.error(msg)
-                raise SetupError(returncode, msg)
-
-        # Remove log directories
-        shutil.rmtree(f"/var/log/{PRODUCT_FAMILY}/sspl", ignore_errors=True)
-        shutil.rmtree(f"/var/log/{PRODUCT_FAMILY}/iem", ignore_errors=True)
-
-        # Remove rsyslog config files
-        self.del_file("/etc/rsyslog.d/0-iemfwd.conf")
-        self.del_file("/etc/rsyslog.d/1-ssplfwd.conf")
-
-        # Remove logrotate config files
-        self.del_file("/etc/logrotate.d/iem_messages")
-        self.del_file("/etc/logrotate.d/sspl_logs")
-
-        # Remove SSPL configuration files
-        shutil.rmtree("/etc/sspl-ll", ignore_errors=True)
-        self.del_file("/etc/sspl.conf.bak")
-
-class SoftReset:
-    """Performs SSPL Soft reset.
-    'soft' is to clean only the data path."""
-
-    name = "soft"
-
-    def process(self):
         # Remove .json files and truncate iem log file
+        HardReset.del_files_from_dir('.json', "/var/cortx/sspl/data/")
         Conf.load(SSPL_CONFIG_INDEX, sspl_config_path)
-        for root, dirs, files in os.walk("/var/cortx/sspl/data/"):
-            for file in files:
-                if file.endswith(".json"):
-                    os.remove(os.path.join(root, file))
         IEM_FILE_PATH = Conf.get(SSPL_CONFIG_INDEX, "IEMSENSOR>log_file_path")
         if os.path.exists(IEM_FILE_PATH):
             with open(IEM_FILE_PATH, 'r+') as f:
