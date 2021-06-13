8699002b47f14af42505b00d3edd7e1e98e96a09
Satish Darade <satish.darade@seagate.com>
2021-06-13 06:17:10 -0600

    EOS-20957:R2 System Health: Storage: FW: Disk groups / Volumes
    Signed-off-by: Satish Darade <satish.darade@seagate.com>

--- low-level/files/opt/seagate/sspl/setup/resource_map/storage.py
+++ low-level/files/opt/seagate/sspl/setup/resource_map/storage.py
@@ -32,6 +32,7 @@ from framework.utils.conf_utils import (
     RACK_ID_KEY, NODE_ID_KEY, CLUSTER_ID_KEY, RELEASE, TARGET_BUILD)
 from error import ResourceMapError
 from resource_map import ResourceMap
+from framework.base.sspl_constants import HEALTH_UNDESIRED_VALS
 
 
 class StorageMap(ResourceMap):
@@ -47,7 +48,9 @@ class StorageMap(ResourceMap):
             "controllers": self.get_controllers_info,
             "psus": self.get_psu_info,
             "platform_sensors": self.get_platform_sensors_info,
-            }
+            "logical_volumes": self.get_logical_volumes_info,
+            "disk_groups": self.get_disk_groups_info
+        }
 
     @staticmethod
     def validate_storage_type_support():
@@ -80,12 +83,14 @@ class StorageMap(ResourceMap):
         rpath: Resouce path (Example: nodes[0]>storage[0]>hw>controllers)
         """
         info = {}
+        fru_found = False
         nodes = rpath.strip().split(">")
         leaf_node, _ = self.get_node_details(nodes[-1])
         if leaf_node == "storage":
             for fru in self.storage_frus:
                 info.update({fru: self.storage_frus[fru]()})
             info["last_updated"] = int(time.time())
+            fru_found = True
         else:
             fru = None
             fru_found = False
@@ -204,6 +209,91 @@ class StorageMap(ResourceMap):
                             sensors_data[platform_sensor] = [single_sensor_data]
         return sensors_data
 
+    def get_logical_volumes_info(self):
+        """Update and return logical volume information in specific format."""
+        logvol_data = []
+        logicalvolumes = self.get_realstor_encl_data("volumes")
+        if logicalvolumes:
+            for logicalvolume in logicalvolumes:
+                health = logicalvolume.get("health", "NA")
+                if health in HEALTH_UNDESIRED_VALS:
+                    health = "NA"
+                logvol_data.append({
+                    "uid": logicalvolume.get("volume-name", "NA"),
+                    "fru": False,
+                    "last_updated": int(time.time()),
+                    "health": {
+                        "status": health,
+                        "description": logicalvolume.get("volume-description", "NA"),
+                        "recommendation": logicalvolume.get("health-recommendation", "NA"),
+                        "specifics": [
+                            {
+                                "disk_group": {
+                                    "disk_group_name": logicalvolume.get("container-name", "NA"),
+                                    "pool_serial_number": logicalvolume.get("container-serial", "NA")
+                                }
+                            }
+                        ]
+                    }
+                })
+        return logvol_data
+
+    def get_disk_groups_info(self):
+        """Update and return disk-group information in specific format."""
+        dg_data = []
+        dg_vol_map = {}
+        diskgroups = self.get_realstor_encl_data("disk-groups")
+        # Mapping logical volumes with disk group.
+        logicalvolumes = self.get_realstor_encl_data("volumes")
+        if logicalvolumes:
+            for logicalvolume in logicalvolumes:
+                volume_pool_sr_no = logicalvolume.get("container-serial", "NA")
+                volume_uid = logicalvolume.get("volume-name", "NA")
+                if volume_pool_sr_no in dg_vol_map:
+                    dg_vol_map[volume_pool_sr_no].append({"volume_uid": volume_uid})
+                else:
+                    dg_vol_map.update({volume_pool_sr_no:[{"volume_uid": volume_uid}]})
+        dg_description = "Disk group is in good health."
+        if diskgroups:
+            for diskgroup in diskgroups:
+                health = diskgroup.get("health", "NA")
+                pool_sr_no = diskgroup.get("pool-serial-number", "NA")
+                if pool_sr_no in dg_vol_map:
+                    volumes = dg_vol_map[pool_sr_no]
+                else:
+                    volumes = None
+                if health in HEALTH_UNDESIRED_VALS:
+                    health = "NA"
+                    dg_description = "Disk group is in unknown health state."
+                dg_data.append({
+                    "uid": "diskgroup-" + diskgroup.get("name", "NA"),
+                    "fru": False,
+                    "last_updated": int(time.time()),
+                    "health": {
+                        "status": health,
+                        "description": dg_description,
+                        "recommendation": diskgroup.get("health-recommendation", "NA"),
+                        "specifics": [
+                            {
+                                "class": diskgroup.get("storage-type", "NA"),
+                                "disks": diskgroup.get("diskcount", "NA"),
+                                "size": diskgroup.get("size", "NA"),
+                                "free": diskgroup.get("freespace", "NA"),
+                                "status": diskgroup.get("status", "NA"),
+                                "current_job": diskgroup.get("current-job", "NA"),
+                                "current_job_completion": diskgroup.get(
+                                    "current-job-completion", "NA"),
+                                "tier": diskgroup.get("storage-tier", "NA"),
+                                "pool": diskgroup.get("pool", "NA"),
+                                "blocksize": diskgroup.get("blocksize", "NA"),
+                                "chunksize": diskgroup.get("chunksize", "NA"),
+                                "volumes": volumes
+                            }
+                        ]
+                    }
+                })
+        return dg_data
+
     @staticmethod
     def get_realstor_encl_data(fru: str):
         """Fetch fru information through webservice API."""
@@ -215,6 +305,8 @@ class StorageMap(ResourceMap):
             "controllers": ENCL.URI_CLIAPI_SHOWCONTROLLERS,
             "power-supplies": ENCL.URI_CLIAPI_SHOWPSUS,
             "platform_sensors": ENCL.URI_CLIAPI_SHOWSENSORSTATUS,
+            "volumes": ENCL.URI_CLIAPI_SHOWVOLUMES,
+            "disk-groups": ENCL.URI_CLIAPI_SHOWDISKGROUPS
         }
         url = ENCL.build_url(fru_uri_map.get(fru))
         response = ENCL.ws_request(url, ENCL.ws.HTTP_GET)
@@ -235,3 +327,7 @@ class StorageMap(ResourceMap):
 #         rpath = f"nodes[0]>storage[0]>hw>{fru}"
 #         health_data = storage.get_health_info(rpath=rpath)
 #         print(health_data)
+#     volume_health_data = storage.get_health_info(rpath="nodes[0]>storage[0]>fw>logical_volumes")
+#     dg_health_data = storage.get_health_info(rpath="nodes[0]>storage[0]>fw>disk_groups")
+#     print(json.dumps(volume_health_data))
+#     print(json.dumps(dg_health_data))
