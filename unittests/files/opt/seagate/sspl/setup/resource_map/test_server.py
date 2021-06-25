2af58ef71ab6880465f6043a4e582b0ff9c3941f
gargpalak048 <76043377+gargpalak048@users.noreply.github.com>
2021-06-25 14:14:25 +0530

    EOS:20926: R2 System Health: Server: HW: Fan (#430)
    *Addressed review comments
    
    *Rebase branch with latest code changes
    
    *Added unittest for Server:HW:Fan module
    
    *Addressed review comments
    
    *Addressed review comments
    
    *Addressed review comments
    
    *Validate server_type before generating health data.
    
    *Fix unittest issues.
    
    *Fix unittest review comments
    
    Signed-off-by: Palak Garg <palak.garg@seagate.com>

--- unittests/files/opt/seagate/sspl/setup/resource_map/test_server.py
+++ unittests/files/opt/seagate/sspl/setup/resource_map/test_server.py
@@ -48,6 +48,15 @@ def get_sdr_type_response(cmd):
     if cmd == "sdr type 'Current'":
         return "", "", 0
 
+    if cmd == "sdr type 'Fan'":
+        return (
+            """ FAN1             | 41h | ok  | 29.1 | 5800 RPM
+                FAN2             | 42h | ok  | 29.2 | 5700 RPM
+                RSC FAN          | 49h | ns  | 29.9 | No Reading""",
+            "",
+            0,
+        )
+
 
 class TestStorageMap(unittest.TestCase):
     def setUp(self) -> None:
@@ -101,6 +110,41 @@ class TestStorageMap(unittest.TestCase):
         sdr_type.side_effect = get_sdr_type_response
         resp = self.server_map.get_platform_sensors_info()
         assert resp["Current"] == []
+
+    @patch("framework.utils.ipmi_client.IPMITool._run_ipmitool_subcommand")
+    def test_build_fan_fru(self, sdr_type):
+        resp = {}
+        sdr_type.side_effect = get_sdr_type_response
+        data = self.server_map.get_fans_info()
+        resp['Fan'] = data
+        assert resp["Fan"][0]["uid"] == "FAN1"
+        assert resp["Fan"][0]["health"]["status"] == "OK"
+        assert (
+            resp["Fan"][0]["health"]["description"]
+            == "FAN1 is in good health."
+        )
+        assert (
+            resp["Fan"][0]["health"]["specifics"][0]["Sensor Reading"]
+            == "5800 RPM"
+        )
+        assert (
+            resp["Fan"][0]["health"]["specifics"][0]["lower_critical_threshold"]
+            == "5.000"
+        )
+        assert (
+            resp["Fan"][0]["health"]["specifics"][0]["upper_critical_threshold"]
+            == "88.000"
+        )
+        assert resp["Fan"][2]["uid"] == "RSC FAN"
+        assert resp["Fan"][2]["health"]["status"] == "NA"
+        assert (
+            resp["Fan"][2]["health"]["description"]
+            == "RSC FAN is not in good health."
+        )
+        assert (
+            resp["Fan"][2]["health"]["recommendation"]
+            == "Please Contact Seagate Support."
+        )
 
 
 if __name__ == "__main__":
