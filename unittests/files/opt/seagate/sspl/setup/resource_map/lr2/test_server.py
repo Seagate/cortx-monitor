07a77438c1cee80002471c1ff345441062a32f4a
Palak Garg <palak.garg@seagate.com>
2021-06-28 04:54:21 -0600

    Avoid multiple creation of class objects in unittest instead use single obj in complete testsuite.
    Signed-off-by: Palak Garg <palak.garg@seagate.com>

--- unittests/files/opt/seagate/sspl/setup/resource_map/lr2/test_server.py
+++ unittests/files/opt/seagate/sspl/setup/resource_map/lr2/test_server.py
@@ -78,14 +78,20 @@ def get_sdr_type_response(cmd):
         )
 
 
-class TestStorageMap(unittest.TestCase):
-    def setUp(self) -> None:
-        self.server_map = ServerMap()
+class TestServerMap(unittest.TestCase):
+    _server_map = None
+
+    @classmethod
+    def create_server_obj(cls):
+        if cls._server_map is None:
+            cls._server_map = ServerMap()
+        return cls._server_map
 
     @patch("framework.utils.ipmi_client.IPMITool._run_ipmitool_subcommand")
     def test_build_platform_sensor_data_temperature(self, sdr_type):
+        obj = self.create_server_obj()
         sdr_type.side_effect = get_sdr_type_response
-        resp = self.server_map.get_platform_sensors_info()
+        resp = obj.get_platform_sensors_info()
         assert resp["Temperature"][0]["uid"] == "CPU1_Temp"
         assert resp["Temperature"][0]["health"]["status"] == "OK"
         assert (
@@ -107,8 +113,9 @@ class TestStorageMap(unittest.TestCase):
 
     @patch("framework.utils.ipmi_client.IPMITool._run_ipmitool_subcommand")
     def test_build_platform_sensor_voltage(self, sdr_type):
+        obj = self.create_server_obj()
         sdr_type.side_effect = get_sdr_type_response
-        resp = self.server_map.get_platform_sensors_info()
+        resp = obj.get_platform_sensors_info()
         assert resp["Voltage"][0]["uid"] == "12V"
         assert resp["Voltage"][0]["health"]["status"] == "OK"
         assert resp["Voltage"][0]["health"]["description"] == "12V sensor is in good health."
@@ -127,15 +134,17 @@ class TestStorageMap(unittest.TestCase):
 
     @patch("framework.utils.ipmi_client.IPMITool._run_ipmitool_subcommand")
     def test_build_platform_sensor_data_current(self, sdr_type):
+        obj = self.create_server_obj()
         sdr_type.side_effect = get_sdr_type_response
-        resp = self.server_map.get_platform_sensors_info()
+        resp = obj.get_platform_sensors_info()
         assert resp["Current"] == []
 
     @patch("framework.utils.ipmi_client.IPMITool._run_ipmitool_subcommand")
     def test_build_fan_fru(self, sdr_type):
+        obj = self.create_server_obj()
         resp = {}
         sdr_type.side_effect = get_sdr_type_response
-        data = self.server_map.get_fans_info()
+        data = obj.get_fans_info()
         resp['Fan'] = data
         assert resp["Fan"][0]["uid"] == "FAN1"
         assert resp["Fan"][0]["health"]["status"] == "OK"
